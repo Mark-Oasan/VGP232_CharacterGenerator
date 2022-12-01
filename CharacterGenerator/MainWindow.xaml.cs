0ba6ccd4f04e62cf5be8e835ae52c2c40d98bd6c
Mark-Oasan <markoasan@gmail.com>
2022-12-01 04:08:04 -0800

    Add project files.

---
+++ CharacterGenerator/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CharacterGenerator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    public partial class MainWindow : Window
+    {
+        private List<string> lstGender = new List<String>();
+        public MainWindow()
+        {
+
+            InitializeComponent();
+            lstGender.Add("Male");
+            lstGender.Add("Female");
+            Cb_Gender.ItemsSource = lstGender;
+
+        }
+    }
+}
